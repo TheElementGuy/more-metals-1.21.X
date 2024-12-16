44957b12e8e48584aff6a37eb3954f2d88c0562e
Jude Rasmussen <jude.a.rasmussen@gmail.com>
2024-12-15 16:36:05 -0800

    Made all cubic zirconia stuff fireproof

--- src/main/java/net/theelementguy/moremetals/block/ModBlocks.java
+++ src/main/java/net/theelementguy/moremetals/block/ModBlocks.java
@@ -17,22 +17,32 @@ import java.util.function.Supplier;
 public class ModBlocks {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MoreMetalsMod.MOD_ID);
 
-    public static final DeferredBlock<Block> CUBIC_ZIRCONIUM_BLOCK = registerBlock("cubic_zirconia_block", () -> new Block(BlockBehaviour.Properties.of().destroyTime(5f).mapColor(DyeColor.WHITE).explosionResistance(6f).sound(SoundType.METAL).requiresCorrectToolForDrops()));
+    public static final DeferredBlock<Block> CUBIC_ZIRCONIUM_BLOCK = registerFireproofBlock("cubic_zirconia_block", () -> new Block(BlockBehaviour.Properties.of().destroyTime(5f).mapColor(DyeColor.WHITE).explosionResistance(6f).sound(SoundType.METAL).requiresCorrectToolForDrops()));
 
-    public static final DeferredBlock<Block> RAW_CUBIC_ZIRCONIUM_BLOCK = registerBlock("raw_cubic_zirconia_block", () -> new Block(BlockBehaviour.Properties.of().destroyTime(5f).mapColor(DyeColor.MAGENTA).explosionResistance(6f).sound(SoundType.AMETHYST).requiresCorrectToolForDrops()));
+    public static final DeferredBlock<Block> RAW_CUBIC_ZIRCONIUM_BLOCK = registerFireproofBlock("raw_cubic_zirconia_block", () -> new Block(BlockBehaviour.Properties.of().destroyTime(5f).mapColor(DyeColor.MAGENTA).explosionResistance(6f).sound(SoundType.AMETHYST).requiresCorrectToolForDrops()));
 
     public static final DeferredBlock<Block> CUBIC_ZIRCONIA_ORE = registerBlock("cubic_zirconia_ore", () -> new Block(BlockBehaviour.Properties.of().destroyTime(3f).mapColor((DyeColor.GRAY)).explosionResistance(3f).sound(SoundType.STONE).requiresCorrectToolForDrops()));
 
     public static final DeferredBlock<Block> DEEPSLATE_CUBIC_ZIRCONIA_ORE = registerBlock("deepslate_cubic_zirconia_ore", () -> new Block(BlockBehaviour.Properties.of().destroyTime(4.5f).mapColor((DyeColor.GRAY)).explosionResistance(3f).sound(SoundType.DEEPSLATE).requiresCorrectToolForDrops()));
 
     public static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
-    DeferredBlock<T> toReturn = BLOCKS.register(name, block);
-    registerBlockItem(name, toReturn);
-    return toReturn;
+        DeferredBlock<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    public static <T extends Block> DeferredBlock<T> registerFireproofBlock(String name, Supplier<T> block) {
+        DeferredBlock<T> toReturn = BLOCKS.register(name, block);
+        registerFireproofBlockItem(name, toReturn);
+        return toReturn;
     }
 
     private static <T extends Block> void registerBlockItem(String name, DeferredBlock<T> block) {
         ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    private static <T extends Block> void registerFireproofBlockItem(String name, DeferredBlock<T> block) {
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().fireResistant()));
     }
 
     public static void register(IEventBus eventBus) {
--- src/main/java/net/theelementguy/moremetals/item/ModToolTiers.java
+++ src/main/java/net/theelementguy/moremetals/item/ModToolTiers.java
@@ -7,6 +7,6 @@ import net.theelementguy.moremetals.util.ModTags;
 
 public class ModToolTiers {
 
-    public static final Tier CUBIC_ZIRCONIA = new SimpleTier(ModTags.Blocks.INCORRECT_FOR_CUBIC_ZIRCONIA_TOOL, 2501, 6.0f, 2.0f, 14, () -> Ingredient.of(ModItems.CUBIC_ZIRCONIA));
+    public static final Tier CUBIC_ZIRCONIA = new SimpleTier(ModTags.Blocks.INCORRECT_FOR_CUBIC_ZIRCONIA_TOOL, 2501, 7.0f, 2.0f, 14, () -> Ingredient.of(ModItems.CUBIC_ZIRCONIA));
 
 }
