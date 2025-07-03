c4e46b3d968993e0098fd8d4a2ba6585efdeb613
Jude Rasmussen <jude.a.rasmussen@gmail.com>
2025-07-02 21:35:54 -0700

    Fix bronze tags...I think (maybe not broken)

--- src/main/java/net/theelementguy/moremetals/datagen/ModBlockTagProvider.java
+++ src/main/java/net/theelementguy/moremetals/datagen/ModBlockTagProvider.java
@@ -3,6 +3,7 @@ package net.theelementguy.moremetals.datagen;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.common.Tags;
 import net.neoforged.neoforge.common.data.BlockTagsProvider;
 import net.theelementguy.moremetals.MoreMetalsMod;
@@ -42,6 +43,10 @@ public class ModBlockTagProvider extends BlockTagsProvider {
         tag(ModTags.Blocks.NEEDS_STARSHARD_TOOL).addTag(Tags.Blocks.NEEDS_NETHERITE_TOOL);
 
         tag(ModTags.Blocks.INCORRECT_FOR_STARSHARD_TOOL).addTag(BlockTags.INCORRECT_FOR_NETHERITE_TOOL).remove(ModTags.Blocks.NEEDS_STARSHARD_TOOL);
+
+        tag(ModTags.Blocks.NEEDS_BRONZE_TOOL).addTag(BlockTags.NEEDS_IRON_TOOL);
+
+        tag(ModTags.Blocks.INCORRECT_FOR_BRONZE_TOOL).addTag(BlockTags.INCORRECT_FOR_IRON_TOOL);
 
     }
 }
