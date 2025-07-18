cac4473daf68f81204ff0922f61ea3a24f4be211
Jude Rasmussen <jude.a.rasmussen@gmail.com>
2025-07-18 09:52:22 -0700

    We have 1.21.8!

--- src/main/java/net/theelementguy/tegmoremetals/MoreMetalsMod.java
+++ src/main/java/net/theelementguy/tegmoremetals/MoreMetalsMod.java
@@ -56,8 +56,6 @@ public class MoreMetalsMod {
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
-        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
-        modContainer.registerConfig(ModConfig.Type.COMMON, MoreMetalsModConfig.SPEC);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event)
